8623cd982b119f0bdd4e5e048eaf89339056bea3
extizout <p.preedee.poom@gmail.com>
2023-03-29 01:31:22 +0700

    Create my conffor nvim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,29 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  {
+    'bluz71/vim-nightfly-colors',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'nightfly'
+    end,
+  },
+
+  { -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = false,
+        theme = 'nightfly',
+        component_separators = '|',
+        section_separators = '',
+      },
+    },
+  },
+
+    'tpope/vim-surround'
+
+}
